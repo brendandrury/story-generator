ab70de991f578670bd06bd66f4f8fab58cc61591
Brendan Drury <brendandrury@g.ucla.edu>
2023-08-22 09:00:34 -0700

    Refactor Character component to use enum and constant array instead of switch statement and strings

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,10 +1,19 @@
 // Import React and useState hook
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+// Define an enum for the genre values
+enum Genre {
+    Fantasy = "Fantasy",
+    ScienceFiction = "Science Fiction",
+    Romance = "Romance",
+    Horror = "Horror",
+    Mystery = "Mystery",
+}
 
 // Define the props type for the Character component
 interface CharacterProps {
     // The genre prop is a string that represents the genre chosen by the user
-    genre: string;
+    genre: Genre;
     // The character prop is a string that represents the character chosen or created by the user
     character: string;
     // The onCharacterChange prop is a function that updates the character prop based on the user's input
@@ -39,85 +48,78 @@ const Character: React.FC<CharacterProps> = ({
     };
 
     // Define an array of predefined characters based on the genre prop
-    let characters: string[];
-    switch (genre) {
-        case "Fantasy":
-            characters = [
-                "A brave knight",
-                "A cunning thief",
-                "A wise wizard",
-                "A beautiful princess",
-                "A fierce dragon",
-                "A loyal dwarf",
-                "A mysterious elf",
-                "A wicked witch",
-                "A noble king",
-                "A humble farmer",
-            ];
-            break;
-        case "Science Fiction":
-            characters = [
-                "A brilliant scientist",
-                "A rogue hacker",
-                "A brave astronaut",
-                "A ruthless alien",
-                "A friendly robot",
-                "A rebellious cyborg",
-                "A cunning spy",
-                "A powerful mutant",
-                "A visionary leader",
-                "A curious explorer",
-            ];
-            break;
-        case "Romance":
-            characters = [
-                "A handsome prince",
-                "A charming lady",
-                "A shy librarian",
-                "A dashing pirate",
-                "A fiery redhead",
-                "A brooding vampire",
-                "A cheerful baker",
-                "A rugged cowboy",
-                "A witty journalist",
-                "A loyal friend",
-            ];
-            break;
-        case "Horror":
-            characters = [
-                "A naive teenager",
-                "A creepy clown",
-                "A paranoid survivor",
-                "A bloodthirsty zombie",
-                "A vengeful ghost",
-                "A psychotic killer",
-                "A haunted doll",
-                "A cursed werewolf",
-                "A sinister cultist",
-                "A terrified victim",
-            ];
-            break;
-        case "Mystery":
-            characters = [
-                "A clever detective",
-                "A cunning criminal",
-                "A loyal sidekick",
-                "A mysterious stranger",
-                "A wealthy heiress",
-                "A shady lawyer",
-                "A nosy reporter",
-                "A eccentric inventor",
-                "A suspicious butler",
-                "A clueless witness",
-            ];
-            break;
-        case "":
-            characters = [];
-            break;
-        default:
-            characters = ["An interesting character"];
-            break;
-    }
+    const characters: Record<Genre, string[]> = {
+        [Genre.Fantasy]: ["A brave knight",
+            "A cunning thief",
+            "A wise wizard",
+            "A beautiful princess",
+            "A fierce dragon",
+            "A loyal dwarf",
+            "A mysterious elf",
+            "A wicked witch",
+            "A noble king",
+            "A humble farmer",
+        ],
+        [Genre.ScienceFiction]: [
+            "A brilliant scientist",
+            "A rogue hacker",
+            "A brave astronaut",
+            "A ruthless alien",
+            "A friendly robot",
+            "A rebellious cyborg",
+            "A cunning spy",
+            "A powerful mutant",
+            "A visionary leader",
+            "A curious explorer",
+        ],
+        [Genre.Romance]: [
+            "A handsome prince",
+            "A charming lady",
+            "A shy librarian",
+            "A dashing pirate",
+            "A fiery redhead",
+            "A brooding vampire",
+            "A cheerful baker",
+            "A rugged cowboy",
+            "A witty journalist",
+            "A loyal friend",
+        ],
+        [Genre.Horror]: [
+            "A naive teenager",
+            "A creepy clown",
+            "A paranoid survivor",
+            "A bloodthirsty zombie",
+            "A vengeful ghost",
+            "A psychotic killer",
+            "A haunted doll",
+            "A cursed werewolf",
+            "A sinister cultist",
+            "A terrified victim",
+        ],
+        [Genre.Mystery]: [
+            "A clever detective",
+            "A cunning criminal",
+            "A loyal sidekick",
+            "A mysterious stranger",
+            "A wealthy heiress",
+            "A shady lawyer",
+            "A nosy reporter",
+            "A eccentric inventor",
+            "A suspicious butler",
+            "A clueless witness",
+        ],
+    };
+
+    // Use useEffect to update the character prop whenever the genre prop changes
+    useEffect(() => {
+        // If the genre prop is valid, choose a random character from the corresponding array
+        if (characters[genre]) {
+            const randomCharacter =
+                characters[genre][Math.floor(Math.random() * characters[genre].length)];
+            // Call the onCharacterChange prop function with the random character as the argument
+            onCharacterChange(randomCharacter);
+        }
+    }, [genre]); // Add genre as a dependency to run useEffect only when genre changes
 
     // Return the JSX element for the Character component
     return (
